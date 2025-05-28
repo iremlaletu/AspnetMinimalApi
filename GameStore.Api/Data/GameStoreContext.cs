b92b11bd439e144bcadd082b41aee8b773119ffd
iremlaletu <iremlaletu@gmail.com>
2025-05-29 00:42:09 +0300

    added seed game

--- GameStore.Api/Data/GameStoreContext.cs
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -13,14 +13,43 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbCo
     {
         modelBuilder.Entity<Genre>()
             .HasData(
-                new  { Id = 1, Name = "Action" },
-                new  { Id = 2, Name = "Adventure" },
-                new  { Id = 3, Name = "Role-Playing" },
-                new  { Id = 4, Name = "Simulation" },
-                new  { Id = 5, Name = "Sports" },
-                new  { Id = 6, Name = "Kids and Family" },
-                new  { Id = 7, Name = "Fighting" },
-                new  { Id = 8, Name = "Racing" }
+                new { Id = 1, Name = "Action" },
+                new { Id = 2, Name = "Adventure" },
+                new { Id = 3, Name = "Role-Playing" },
+                new { Id = 4, Name = "Simulation" },
+                new { Id = 5, Name = "Sports" },
+                new { Id = 6, Name = "Kids and Family" },
+                new { Id = 7, Name = "Fighting" },
+                new { Id = 8, Name = "Racing" }
             );
+
+        modelBuilder.Entity<Game>()
+            .HasData(
+                new
+                {
+                    Id = 1,
+                    Name = "The Witcher 3",
+                    Price = 49.99m,
+                    ReleaseDate = new DateOnly(2015, 5, 19),
+                    GenreId = 3 // Role-Playing
+                },
+                new
+                {
+                    Id = 2,
+                    Name = "Stardew Valley",
+                    Price = 14.99m,
+                    ReleaseDate = new DateOnly(2016, 2, 26),
+                    GenreId = 4 // Simulation
+                },
+                new
+                {
+                    Id = 3,
+                    Name = "Celeste",
+                    Price = 19.99m,
+                    ReleaseDate = new DateOnly(2018, 1, 25),
+                    GenreId = 1 // Action
+                }
+        );
+
     }
 }
---
+++ GameStore.Api/Data/Migrations/20250528213653_SeedGames.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GameStore.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedGames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Games",
+                columns: new[] { "Id", "GenreId", "Name", "Price", "ReleaseDate" },
+                values: new object[,]
+                {
+                    { 1, 3, "The Witcher 3", 49.99m, new DateOnly(2015, 5, 19) },
+                    { 2, 4, "Stardew Valley", 14.99m, new DateOnly(2016, 2, 26) },
+                    { 3, 1, "Celeste", 19.99m, new DateOnly(2018, 1, 25) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
